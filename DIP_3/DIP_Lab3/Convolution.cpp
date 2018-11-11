3c85595ecfb954f7caa791ab6bdfdb8ff083f8d9
NhatHuyTran <nhathuy13598@gmail.com>
2018-11-11 23:47:34 +0700

    Viet xong lop Convolution

--- DIP_3/DIP_Lab3/Convolution.cpp
+++ DIP_3/DIP_Lab3/Convolution.cpp
@@ -13,6 +13,28 @@ void Convolution::SetKernel(vector<float> kernel, int kWidth, int kHeight) {
 }
 
 int Convolution::DoConvolution(const Mat& sourceImage, Mat& destinationImage) {
+	// Giả định rằng ma trận có kích thước là số lẻ và là ma trận vuông
+	int rows = sourceImage.rows;
+	int cols = sourceImage.cols;
+	int kernelIndex = _kernelWidth / 2;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			int sum = 0;
+			for (int s = -kernelIndex; s <= kernelIndex; s++) {
+				for (int t = -kernelIndex; t <= kernelIndex; t++) {
+					int indexX = s + kernelIndex;
+					int indexY = t + kernelIndex;
+					int index = _kernelWidth * indexX + indexY;
+					if (i - s >= 0 && j - t >= 0 && i - s < rows && j - t < cols) {
+						sum += int(_kernel[index] * sourceImage.at<uchar>(i - s, j - t));
+						
+					}
+					
+				}
+			}
+			destinationImage.at<uchar>(i,j) = sum;
+		}
+	}
 	return 1;
 }
 
--- DIP_3/DIP_Lab3/Source.cpp
+++ DIP_3/DIP_Lab3/Source.cpp
@@ -8,20 +8,28 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	Mat inputImage = imread("grayLena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat outputImage;
-	//copyMakeBorder(inputImage, outputImage, 100 - 1, 100 - 1, 100 - 1, 100 - 1, 0);
-	//uchar* data = (uchar*)outputImage.data;
-	//cout << int(data[0]);
-	//cout << outputImage;
+	//Mat inputImage = imread("grayLena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	//Mat outputImage;
+	////copyMakeBorder(inputImage, outputImage, 100 - 1, 100 - 1, 100 - 1, 100 - 1, 0);
+	////uchar* data = (uchar*)outputImage.data;
+	////cout << int(data[0]);
+	////cout << outputImage;
 
-	Blur bl;
-	bl.BlurImage(inputImage, outputImage, 3, 3, 1);
-	imwrite("output.jpg", outputImage);
-	namedWindow("Input image");
-	imshow("Input image", inputImage);
-	namedWindow("Output image");
-	imshow("Output image", outputImage);
+	//Blur bl;
+	//bl.BlurImage(inputImage, outputImage, 3, 3, 1);
+	//imwrite("output.jpg", outputImage);
+	//namedWindow("Input image");
+	//imshow("Input image", inputImage);
+	//namedWindow("Output image");
+	//imshow("Output image", outputImage);
+	Mat a = Mat(5, 5, CV_8UC1 ,Scalar(0));
+	Mat b = Mat(5, 5, CV_8UC1);
+	a.at<uchar>(2, 2) = 1;
+	vector<float> temp = { 1,2,3,4,5,6,7,8,9 };
+	Convolution cv;
+	cv.SetKernel(temp, 3, 3);
+	cv.DoConvolution(a, b);
+	cout << b << endl;
 	waitKey(0);
 	return 0;
 }
